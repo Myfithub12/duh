d3c560b3c80365216ac8322010f3b1f79936f92e
Mark <mhollrah01@yahoo.com>
2022-10-30 12:51:55 -0400

    10/30/22

--- javascript/site.js
+++
@@ -1,9 +0,0 @@
-let rotation = 0;
-function rotateImg() {
-  rotation += 90; // add 90 degrees, you can change this as you want
-    if (rotation === 360) { 
-    // 360 means rotate back to 0
-    rotation = 0;
-    }
-    document.querySelector("#img").style.transform = `rotate(${rotation}deg)`;
-}
