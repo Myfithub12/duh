a1d090027e3303693591af5ee79c67f518209980
Mark <mhollrah01@yahoo.com>
2022-11-05 12:10:23 -0400

    11/5/22

---
+++ css/jkl.css
@@ -0,0 +1,39 @@
+body {
+    margin: 0;
+    font-family: var(--bs-font-sans-serif);
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #1d5791;
+    background-color: #fff;
+    -webkit-text-size-adjust: 100%;
+    -webkit-tap-highlight-color: rgba(170, 140, 140, 0);
+}
+
+header {
+    background-color: whitesmoke;
+}
+
+footer {
+    background-color: white;
+}
+
+#row {
+    text-align: center;
+}
+
+#col {
+    text-align: center;
+}
+
+.name {
+    text-align: center;
+    color: purple;
+}
+
+h6, .h6, h5, .h5, h4, .h4, h3, .h3, h2, .h2, h1, .h1 {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    line-height: 1.2;
+}
